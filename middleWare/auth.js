524eb5c48bcb20dc76cdcccc1141958c6d2c17d1
Shafeeque Rahman O <shafeeque.rahman@claysys.net>
2022-04-13 11:22:14 +0530

    test

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -10,7 +10,7 @@ const auth = async(req,res,next)=>{
        const user = await User.findOne({_id:decoded._id, 'tokens.token':token})
 
        if(!user){
-           res.send()
+           res.send("ERROR")
        }
        req.user =user
        next()
